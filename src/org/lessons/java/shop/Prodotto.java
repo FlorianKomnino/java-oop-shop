3db711358b90a53116019cf2a206070530d1b725
FlorianKomnino <florian.komnino@outlook.it>
2023-05-08 16:01:14 +0200

    completed exercise with bonus, getPrezzoIvato() not working

--- src/org/lessons/java/shop/Prodotto.java
+++ src/org/lessons/java/shop/Prodotto.java
@@ -1,5 +1,57 @@
 package org.lessons.java.shop;
 
-public class Prodotto {
+import java.util.Random;
 
+public class Prodotto {
+	Random rnd = new Random();
+	private int codice;
+	private String nome;
+	private String descrizione;
+	private Float prezzo;
+	private int iva;
+	
+	public Prodotto(String nome, float prezzo, int iva) {
+		codice = rnd.nextInt(1000000);
+		this.nome = nome;
+		this.prezzo = prezzo;
+		this.iva = iva;
+	}
+	
+	public String getCodice() {
+		return String.format("%08d", codice);
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setDescrizione(String descrizione) {
+		this.descrizione = descrizione;
+	}
+	public String getDescrizione() {
+		return descrizione;
+	}
+	public void setPrezzo(Float prezzo) {
+		this.prezzo = prezzo;
+	}
+	public float getPrezzo() {
+		return prezzo;
+	}
+	public void setIva(int iva) {
+		this.iva= iva;
+	}
+	public int getIva() {
+		return iva;
+	}
+	
+	public float getPrezzoBase() {
+		return getPrezzo();
+	}
+	public float getPrezzoIvato() {
+		return getPrezzo() * (1 + (getIva() / 100));
+	}
+	public String getNomeEsteso() {
+		return "Il nome completo del prodotto e' :" + getCodice() + " " + nome;
+	}
 }
